c53478e025f28b138c76c8b5e339c15bf62a4670
Hennadii <genchik.98.vn@gmail.com>
2020-07-20 23:57:53 +0300

    heroku deploy

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const Main = () => {
-    return "<>"+
-        "<h1>Hello World</h1>"+
-        "</>";
-};
-console.log('index js file');
-export default Main;
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: 'postgres://bbvkyfiioynfhu:d8cecdbf914e2c83243079911d844f1a1a5b1a44ec7ccab801c4b9e1ecc589c8@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d2lfrklo6dv0rm',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+//console.log(process.env.DATABASE_URL);
+client.connect();
+
+
+const fastify = require('fastify')({logger: true});
+fastify.route({
+    method: 'GET',
+    url: '/api',
+    schema: {
+        // request needs to have a querystring with a `name` parameter
+        querystring: {
+            name: { type: 'string' }
+        },
+        // the response needs to be an object with an `hello` property of type 'string'
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    },
+    preHandler: async (request, reply) => {
+        fastify.log.info('prehendler message');
+    },
+    handler: async (request, reply) => {
+        return { hello: 'world' }
+    }
+
+});
+const start = async () => {
+    try {
+        await fastify.listen(3000);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (e) {
+        fastify.log.error(e)
+        process.exit(1)
+    }
+}
+start();
