6d824f794b35e7b375067d97b35541f8ac2611eb
Hennadii <genchik.98.vn@gmail.com>
2020-07-31 15:45:21 +0300

    deploy

--- pages/api/main.js
+++ pages/api/main.js
@@ -43,14 +43,11 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
     try {
-        await fastify.listen(process.env.PORT || 3000, function (err) {
-            if (err) throw err
-            console.log(`server listening on ${fastify.server.address().port}`)
-        })
-        fastify.log.info(`server listening on main.js`);
-    } catch (e) {
-        fastify.log.error(e)
+        await fastify.listen(3000)
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err)
         process.exit(1)
     }
 }
-start();
+start()
